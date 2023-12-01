202bb50739d2bd64cf398478bbbcdbce6fb67977
Sandro Vita <s.vita@tlco.it>
2023-12-01 14:37:22 +0100

    removed target token from required in move method

--- move_repo.py
+++ move_repo.py
@@ -169,7 +169,6 @@ async def move_repository(source_platform: str, target_platform: str, repo_obj: 
             repo_obj.source_repo_url, 
             target_platform, 
             repo_obj.repo_name, 
-            repo_obj.target_token, 
             repo_obj.organization, 
             repo_obj.project
         )
